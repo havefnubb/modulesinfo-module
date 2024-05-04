f2e9f399bfb1b287f15aaa6e968792579c25af27
Laurent Jouanneau <dev@ljouanneau.com>
2024-05-04 10:19:56 +0200

    Add listener to register a menu item into the adminui interface

--- modulesinfo/classes/modulesinfo.listener.php
+++ modulesinfo/classes/modulesinfo.listener.php
@@ -4,12 +4,17 @@
 * @subpackage modulesinfo
 * @author    FoxMaSk
 * @contributor Laurent Jouanneau
-* @copyright 2008-2011 FoxMaSk, 2010 laurent jouanneau
+* @copyright 2008-2011 FoxMaSk, 2010-2024 laurent jouanneau
 * @link      https://havefnubb.jelix.org
 * @licence  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public Licence, see LICENCE file
 */
+use Jelix\AdminUI\SideBar\JelixLinkMenuItem;
 
-class modulesinfoListener extends jEventListener{
+class modulesinfoListener extends jEventListener
+{
+    protected $eventMapping = array(
+        'adminui.loading' => 'onAdminUILoading',
+    );
 
     function onmasteradminGetMenuContent ($event) {
 
@@ -21,6 +26,22 @@ class modulesinfoListener extends jEventListener{
                                             'system');
             $item->icon = \jUrl::get('jelix~www:getfile', array('targetmodule' => 'modulesinfo', 'file' => 'modules_list.png'));
             $event->add($item);
+        }
+    }
+
+    /**
+     * @param jEvent $event
+     */
+    public function onAdminUILoading($event)
+    {
+        if ( jAcl2::check('modulesinfo.access')) {
+            /** @var \Jelix\AdminUI\UIManager $uim */
+            $uim = $event->uiManager;
+            $uim->sidebar()->getSubMenu('system')->addMenuItem(new JelixLinkMenuItem(
+                jLocale::get('modulesinfo~modulesinfo.masteradmin.menu.item'),
+                'modulesinfo~default:index', null,
+                'cube'
+            ));
         }
     }
 }
